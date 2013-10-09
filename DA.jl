ac902c0de4b549ff944a6d9a7d7081f364f4e2c8
trthatcher <tim.russell.thatcher@gmail.com>
2013-10-08 23:18:03 -0400

    Further progress

--- DA.jl
+++ DA.jl
@@ -5,23 +5,72 @@ data = readcsv("iris.csv")
 predictor = data[:,1:4]
 
 classes =  data[:,5] # map(x -> convert(Integer x), data[:,5])
-
 classes = map(x -> convert(Integer, x), classes)
 
+
+gamma = 0
+lambda = 0.5
+
 classlist = unique(classes)
-
 k = length(classlist)
+n,p = size(predictor)
 
-p, n = size(predictor)
+sigma_k = Array(Float64, p, p, k)
+mu_k = Array(Float64, p, k)
+priors = Array(Float64, k)
 
-#Pooled group is group 0
+sigma = Array(Float64, p, p)
+sigma = cov(predictor)
 
-cov_mat = Array(Float64, p, p, k + 1)
-
-mean_mat = Array(Float64, p, k)
 
 for i in (1:k)
+	class_indices = find(classes .== i)
+	priors[i] = length(class_indices) / n
+	mu_k[:,i] = vec(mean(predictor[class_indices,:], 1))
+	sigma_k[:,:,i] = cov(predictor[class_indices,:])
+end
+
+sigma2_k = Array(Float64, p, p, k)
+
+# Approximately correct for now
+for i in (1:k)
+	class_indices = find(classes .== i)
+	mu_k[:,i] = vec(mean(predictor, 1))
+	sigma2_k[:,:,i]	= (1-lambda) * sigma_k[:,:,i] + lambda * sigma
+end
+
+
+#############################
+
+classlist = unique(classes)
+k = length(classlist)
+n,p = size(predictor)
+
+mu_k = Array(Float64, p, k)
+priors = Array(Float64, k)
+
+sigma = Array(Float64, p, p)
+sigma = cov(predictor)
+
+
+
+if lambda == 1 # LDA
+	mu_k[:,i] = vec(mean(predictor[class_indices,:], 1))
+
+else
+	sigma_k = Array(Float64, p, p, k)
+	if lambda == 0 # QDA
+
+	else # RDA
+		for i in 1:k
+			class_k = find(classes .== i)
+			mu_k[:,i] = mean(predictor[class_k,:],1)
+			for j in 1:n
+				
+			end
+		end
 
 end
+
 
 
