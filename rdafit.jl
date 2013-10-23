d6f67ef08a5f7ac9b0f69f23f4d6b95ff0183a84
trthatcher <tim.russell.thatcher@gmail.com>
2013-10-22 23:30:56 -0400

    Change fitting mechanism and changing types

--- rdafit.jl
+++ rdafit.jl
@@ -1,49 +1,85 @@
 type DaResp{T<:FP} <: ModResp
 	y::PooledDataArray	# Response vector
 	priors::Vector{T}	# Prior weights
+	counts::Vector{Int64}	# Prior observation counts
 	function DaResp(y::PooledDataArray, priors::Vector{T})
-		length(priors) == length(levels(y)) || error("Length mismatch")
-		new(y, priors)
+		length(priors) == length(levels(y)) || error("Length mismatch priors/levels")
+		n = length(y)
+		c = zeros(Int64,n)
+		c[y.refs[i]] += 1 for i = 1:n
+		new(y, priors, c)
+	end
+end
+
+type RDisAnalysis{T<:FP} <: DisAnalysis
+	X::Matrix{T}
+	means::Matrix{T}
+	whiten::Array{T,3}
+	logpr::Vector{T}
+	lambda::T
+	gamma::Real
+	function RDisAnalysis(r::DaResp, X::Matrix{T}, lambda::Float64, gamma::Real=0)
+		k = length(r.counts)
+		n, p = size(X)
+		means = groupmeans(r, X)
+		whiten = Array(Float64,p,p,k)
+		logpr = log(r.priors)
+		new(X,means,whiten,logpr,lambda,gamma)
+	end
+end
+
+type LDisAnalysis{T<:FP} <: DisAnalysis
+	X::Matrix{T}
+	means::Matrix{T}
+	whiten::Matrix{T}
+	logpr::Vector{T}
+	gamma::Real
+	function RDisAnalysis(r::DaResp, X::Matrix{T}, gamma::Real=0)
+		k = length(r.counts)
+		n, p = size(X)
+		means = groupmeans(r, X)
+		whiten = Array(Float64,p,p)
+		logpr = log(r.priors)
+		new(X,means,whiten,logpr,gamma)
+	end
+end
+
+type QDisAnalysis{T<:FP} <: DisAnalysis
+	X::Matrix{T}
+	means::Matrix{T}
+	whiten::Array{T,3}
+	logpr::Vector{T}
+	gamma::Real
+	function RDisAnalysis(r::DaResp, X::Matrix{T}, gamma::Real=0)
+		k = length(r.counts)
+		n, p = size(X)
+		means = groupmeans(r, X)
+		whiten = Array(Float64,p,p,k)
+		logpr = log(r.priors)
+		new(X,means,whiten,logpr,gamma)
 	end
 end
 
 
-
-type RDisAnalysis{T<:FP} <: DisAnalysis
-	means::Matrix{T}
-	whiten::Array{T,3}
-	logpr::Vector{T}
-end
-
-type LDisAnalysis{T<:FP} <: DisAnalysis
-	means::Matrix{T}
-	whiten::Matrix{T}
-	logpr::Vector{T}
-end
-
 type RdaMod <: DisAnalysisModel
 	fr::ModelFrame
-	rr::DaResp
+	dr::DaResp
 	da::RDisAnalysis
 	ff::Formula
-	gg::Real
-	ll::Real
 end
 
 type LdaMod <: DisAnalysisModel
 	fr::ModelFrame
-	rr::DaResp
+	dr::DaResp
 	da::LDisAnalysis
 	ff::Formula
-	gg::Real
 end
 
 type QdaMod <: DisAnalysisModel
 	fr::ModelFrame
-	rr::DaResp
-	da::RDisAnalysis
+	dr::DaResp
+	da::QDisAnalysis
 	ff::Formula
-	gg::Real
 end
 
 
@@ -54,27 +90,27 @@ end
 
 # Find group means
 # Don't pass NAs in the PDA or index 0 will be accessed
-function groupmeans{T<:FP}(y::PooledDataVector, X::Matrix{T})
-	n,p = size(X); k = length(levels(y))
-	length(y) == n || error("Array lengths do not conform")
-	g = zeros(T, k, p); nk = zeros(Int64, k)
+function groupmeans{T<:FP}(r::DaResp, X::Matrix{T})
+	n,p = size(X)
+	k = length(r.counts)
+	length(r.y) == n || error("Array lengths do not conform")
+	g = zeros(T, k, p)
 	for i = 1:n
-		g[y.refs[i],:] += X[y.refs[i], :]
-		nk[y.refs[i]] += 1
+		g[r.y.refs[i],:] += X[r.y.refs[i], :]
 	end
 	for i = 1:k
-		g[i,:] = g[i,:] / nk[i]
+		g[i,:] = g[i,:] / r.counts[i]
 	end
-	return (nk, g)
+	return g
 end
 
 # Center matrix by group mean
-function centermatrix{T<:FP}(X::Matrix{T}, g::Matrix{T},y::PooledDataVector)
+function centermatrix{T<:FP, U<:Integer}(X::Matrix{T}, M::Matrix{T}, index::Vector{U})
 	n,p = size(X)
 	Xc = Array(Float64,n,p)
 	sd = zeros(Float64,1,p)
 	for i = 1:n
-		Xc[i,:] = X[i,:] - g[y.refs[i],:]
+		Xc[i,:] = X[i,:] - M[index[i],:]
 		sd += Xc[i,:].^2
 	end
 	sd = sqrt(sd/(n-1))
@@ -122,16 +158,14 @@ function fitrda{T<:FP}(rr::DaResp,X::Matrix{T},lambda::T,gamma::Real)
 end
 
 # Rework to have response
-function ssfitrda{T<:FP}(rr::DaResp,X::Matrix{T},lambda::T,gamma::Real)
-	ng::Int64 = length(levels(rr.y))
-	n::Int64, p::Int64 = size(X)
-	nk::Vector{Int64}, mu_k = groupmeans(rr.y, X)
-	Xc::Matrix{Float64}, sd::Vector{Float64} = centermatrix(X, mu_k, rr.y)
+function fitda!{T<:FP}(dr::DaResp, da::RDisAnalysis)
+	ng = length(levels(dr.counts))
+	n, p = size(da.X)
+	Xc, sd = centermatrix(da.X,da.means,dr.y.refs)
 	Sigma = transpose(Xc) * Xc	# NOTE: Not weighted with n-1
 	Sigma_k = Array(Float64,p,p)
-	whiten_k = Array(Float64,p,p,ng)
 	for k = 1:ng	# BLAS/LAPACK optimizations?
-		class_k = find(rr.y.refs .== k)
+		class_k = find(dr.y.refs .== k)
 		Sigma_k = transpose(Xc[class_k,:]) * Xc[class_k,:]
 		Sigma_k = (1-lambda) * Sigma_k + lambda * Sigma		# Shrink towards Pooled covariance
 		s, V = svd(Sigma_k)[2:3]
@@ -139,9 +173,8 @@ function ssfitrda{T<:FP}(rr::DaResp,X::Matrix{T},lambda::T,gamma::Real)
 		if gamma != 0
 			s = s .* (1-gamma) .+ (gamma * trace(Sigma_k) / p)	# Shrink towards (I * Average Eigenvalue)
 		end
-		whiten_k[:,:,k] = diagm(1 ./ sd) * V * diagm(1 ./ sqrt(s))
+		da.whiten[:,:,k] = diagm(1 ./ sd) * V * diagm(1 ./ sqrt(s))
 	end
-	return (mu_k,whiten_k)
 end
 
 
