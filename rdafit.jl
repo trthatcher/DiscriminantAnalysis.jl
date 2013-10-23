9d2f91ab4af341308d746467a291ef75be155529
trthatcher <tim.russell.thatcher@gmail.com>
2013-10-23 00:45:55 -0400

    Bug fixes

--- rdafit.jl
+++ rdafit.jl
@@ -3,10 +3,13 @@ type DaResp{T<:FP} <: ModResp
 	priors::Vector{T}	# Prior weights
 	counts::Vector{Int64}	# Prior observation counts
 	function DaResp(y::PooledDataArray, priors::Vector{T})
-		length(priors) == length(levels(y)) || error("Length mismatch priors/levels")
+		k = length(priors)
+		k == length(levels(y)) || error("Length mismatch priors/levels")
 		n = length(y)
-		c = zeros(Int64,n)
-		c[y.refs[i]] += 1 for i = 1:n
+		c = zeros(Int64,k)
+		for i = 1:n
+			c[y.refs[i]] += 1
+		end
 		new(y, priors, c)
 	end
 end
@@ -18,7 +21,7 @@ type RDisAnalysis{T<:FP} <: DisAnalysis
 	logpr::Vector{T}
 	lambda::T
 	gamma::Real
-	function RDisAnalysis(r::DaResp, X::Matrix{T}, lambda::Float64, gamma::Real=0)
+	function RDisAnalysis(r::DaResp, X::Matrix{T}, lambda::T, gamma=0)
 		k = length(r.counts)
 		n, p = size(X)
 		means = groupmeans(r, X)
@@ -34,7 +37,7 @@ type LDisAnalysis{T<:FP} <: DisAnalysis
 	whiten::Matrix{T}
 	logpr::Vector{T}
 	gamma::Real
-	function RDisAnalysis(r::DaResp, X::Matrix{T}, gamma::Real=0)
+	function RDisAnalysis(r::DaResp, X::Matrix{T}, gamma=0)
 		k = length(r.counts)
 		n, p = size(X)
 		means = groupmeans(r, X)
@@ -50,7 +53,7 @@ type QDisAnalysis{T<:FP} <: DisAnalysis
 	whiten::Array{T,3}
 	logpr::Vector{T}
 	gamma::Real
-	function RDisAnalysis(r::DaResp, X::Matrix{T}, gamma::Real=0)
+	function RDisAnalysis(r::DaResp, X::Matrix{T}, gamma=0)
 		k = length(r.counts)
 		n, p = size(X)
 		means = groupmeans(r, X)
@@ -124,42 +127,10 @@ end
 # %~%~%~%~%~%~%~%~%~% Fitting Functions %~%~%~%~%~%~%~%~%
 
 
-# Fit LDA
-function fitlda()
-	true
-end
-
-# Fit QDA
-function fitqda()
-	true
-end
-
-# Fit RDA 
-function fitrda{T<:FP}(rr::DaResp,X::Matrix{T},lambda::T,gamma::Real)
-	ng::Int64 = length(levels(rr.y))
-	n::Int64, p::Int64 = size(X)
-	nk::Vector{Int64}, mu_k = groupmeans(rr.y, X)
-	Xc::Matrix{Float64}, sd::Vector{Float64} = centermatrix(X, mu_k, rr.y)
-	Sigma = transpose(Xc) * Xc	# NOTE: Not weighted with n-1
-	Sigma_k = Array(Float64,p,p)
-	whiten_k = Array(Float64,p,p,ng)
-	for k = 1:ng	# BLAS/LAPACK optimizations?
-		class_k = find(rr.y.refs .== k)
-		Sigma_k = transpose(Xc[class_k,:]) * Xc[class_k,:]
-		Sigma_k = (1-lambda) * Sigma_k + lambda * Sigma		# Shrink towards Pooled covariance
-		s, V = svd(Sigma_k)[2:3]
-		s = s ./ ((1-lambda)*(nk[k]-1) + lambda*(n-ng))
-		if gamma != 0
-			s = s .* (1-gamma) .+ (gamma * trace(Sigma_k) / p)	# Shrink towards (I * Average Eigenvalue)
-		end
-		whiten_k[:,:,k] = diagm(1 ./ sd) * V * diagm(1 ./ sqrt(s))
-	end
-	return (mu_k,whiten_k)
-end
-
 # Rework to have response
-function fitda!{T<:FP}(dr::DaResp, da::RDisAnalysis)
-	ng = length(levels(dr.counts))
+function fitda!(dr::DaResp, da::RDisAnalysis)
+	println(dr)
+	ng = length(dr.counts)
 	n, p = size(da.X)
 	Xc, sd = centermatrix(da.X,da.means,dr.y.refs)
 	Sigma = transpose(Xc) * Xc	# NOTE: Not weighted with n-1
@@ -167,11 +138,11 @@ function fitda!{T<:FP}(dr::DaResp, da::RDisAnalysis)
 	for k = 1:ng	# BLAS/LAPACK optimizations?
 		class_k = find(dr.y.refs .== k)
 		Sigma_k = transpose(Xc[class_k,:]) * Xc[class_k,:]
-		Sigma_k = (1-lambda) * Sigma_k + lambda * Sigma		# Shrink towards Pooled covariance
-		s, V = svd(Sigma_k)[2:3]
-		s = s ./ ((1-lambda)*(nk[k]-1) + lambda*(n-ng))
-		if gamma != 0
-			s = s .* (1-gamma) .+ (gamma * trace(Sigma_k) / p)	# Shrink towards (I * Average Eigenvalue)
+		Sigma_k = (1-da.lambda) * Sigma_k + da.lambda * Sigma		# Shrink towards Pooled covariance		
+		s::Vector{Float64}, V::Matrix{Float64} = svd(Sigma_k)[2:3]
+		s = s ./ ((1-da.lambda)*(dr.counts[k]-1) + da.lambda*(n-ng))
+		if da.gamma != 0
+			s = s .* (1-da.gamma) .+ (da.gamma * trace(Sigma_k) / p)	# Shrink towards (I * Average Eigenvalue)
 		end
 		da.whiten[:,:,k] = diagm(1 ./ sd) * V * diagm(1 ./ sqrt(s))
 	end
@@ -183,25 +154,28 @@ end
 # %~%~%~%~%~%~%~%~%~% Wrapper Functions %~%~%~%~%~%~%~%~%
 
 
-function rda(f::Formula, df::AbstractDataFrame; priors::Vector{Float64}=Float64[], lambda::Real=0.5, gamma::Real=0)
+function rda(f::Formula, df::AbstractDataFrame; priors::Vector{Float64}=Float64[], lambda=0.5, gamma=0)
 	mf::ModelFrame = ModelFrame(f, df)
 	mm::Matrix{Float64} = ModelMatrix(mf).m[:,2:]
 	y::PooledDataVector = PooledDataArray(model_response(mf)) 	# NOTE: pooled conversion done INSIDE rda in case df leaves out factor levels
-	n = length(y); k = length(levels(y)); lp = length(priors)
-	lp == 0 || lp == k || error("length(priors) = $lp should be 0 or $k")
-	p = lp == k ? copy(priors) : ones(Float64, k)/k
-	rr = DaResp{Float64}(y, p)
+	n = length(y)
+	k = length(levels(y))
+	lp = length(priors)
+		lp == 0 || lp == k || error("length(priors) = $lp should be 0 or $k")
+	pr = lp == k ? copy(priors) : ones(Float64, k)/k
+	dr = DaResp{Float64}(y, pr)
 	if lambda == 1
-		da = fitlda()
-		res = LdaMod(mf,rr,da,f,lambda,gamma)
+		#da = fitlda()
+		#res 
 	elseif lambda == 0
-		da = fitqda()
-		res = QdaMod(mf,rr,da,f,lambda,gamma)
+		#da = fitqda()
+		#res
 	else
-		a,b = fitrda(rr,mm, lambda, gamma)
-		da = RDisAnalysis(a, b, log(priors))
-		res = (rr, da) #RdaMod(mf,rr,da,f,lambda,gamma)
+		da = RDisAnalysis{Float64}(dr, mm, lambda, gamma)
+		fitda!(dr,da)
+		res = RdaMod(mf,dr,da,f)
 	end
+	return res
 end
 
 # Multiple Dispatch and alternatives
