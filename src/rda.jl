34eec1e5e598d19689566e6a481f4b3ba97b5d45
Tim Thatcher <tim.russell.thatcher@gmail.com>
2015-11-18 09:46:53 -0500

    Added several helper functions

--- src/rda.jl
+++ src/rda.jl
@@ -11,6 +11,24 @@ function translate!{T<:AbstractFloat}(A::Array{T}, b::T)
 end
 translate!{T<:AbstractFloat}(b::T, A::Array{T}) = translate!(A, b)
 
+# A := A .+ b'
+function translate!{T<:AbstractFloat}(b::Vector{T}, A::Matrix{T})
+    (n = size(A,1)) == length(b) || throw(DimensionMismatch("first dimension of A does not match length of b"))
+    @inbounds for j = 1:size(A,2), i = 1:n
+        A[i,j] += b[i]
+    end
+    A
+end
+
+# A := b .+ A
+function translate!{T<:AbstractFloat}(A::Matrix{T}, b::Vector{T})
+    (n = size(A,2)) == length(b) || throw(DimensionMismatch("second dimension of A does not match length of b"))
+    @inbounds for j = 1:n, i = 1:size(A,1)
+        A[i,j] += b[j]
+    end
+    A
+end
+
 # S1 := (1-λ)S2 + λ
 function regularize!{T<:AbstractFloat}(S1::Matrix{T}, λ::T, S2::Matrix{T})
     (n = size(S1,1)) == size(S1,2) || throw(DimensionMismatch("Matrix S1 must be square."))
@@ -22,6 +40,16 @@ function regularize!{T<:AbstractFloat}(S1::Matrix{T}, λ::T, S2::Matrix{T})
     end
     S1
 end
+
+# Symmetrize the lower half of matrix S using the upper half of S
+function symml!(S::Matrix)
+    (p = size(S,1)) == size(S,2) || throw(ArgumentError("S must be square"))
+    @inbounds for j = 1:(p - 1), i = (j + 1):p 
+        S[i, j] = S[j, i]
+    end
+    S
+end
+symml(S::Matrix) = symml!(copy(S))
 
 function class_counts{T<:Integer}(y::Vector{T}, k::T = maximum(y))
     counts = zeros(Int64, k)
@@ -42,6 +70,13 @@ function class_totals{T<:AbstractFloat,U<:Integer}(X::Matrix{T}, y::Vector{U}, k
     M
 end
 
+#=
+function class_means{T<:AbstractFloat,U<:Integer}(X::Matrix{T}, y::Vector{U}, 
+=#
+
+# Center rows of X based on class mean in M
+#   X is uncentered data matrix
+#   M is matrix of class means (one per row)
 function center_rows!{T<:AbstractFloat,U<:Integer}(X::Matrix{T}, M::Matrix{T}, y::Vector{U})
     n, p = size(X)
     for j = 1:p, i = 1:n
@@ -51,6 +86,8 @@ function center_rows!{T<:AbstractFloat,U<:Integer}(X::Matrix{T}, M::Matrix{T}, y
 end
 
 # Create an array of class scatter matrices
+#   H is centered data matrix (with respect to class means)
+#   y is one-based vector of class IDs
 function class_covariances{T<:AbstractFloat,U<:Integer}(H::Matrix{T}, y::Vector{U}, 
                                                         n_k::Vector{Int64} = class_counts(y))
     k = length(n_k)
@@ -58,12 +95,15 @@ function class_covariances{T<:AbstractFloat,U<:Integer}(H::Matrix{T}, y::Vector{
     Σ_k = Array(Array{T,2}, k)
     for i = 1:k  # Σ_k[i] = H_i'H_i/(n_i-1)
         Σ_k[i] = BLAS.syrk!('U', 'T', one(T)/(n_k[i]-1), H[y .== i,:], zero(T), Array(T,p,p))
+        symml!(Σ_k)
     end
     Σ_k
 end
 
-# Use eigendecomposition to generate class whitening matrices
-function class_whiteners!{T<:AbstractFloat}(Σ_k::Array{Array{T,2},1}, γ::T)
+# Use eigendecomposition to generate class whitening transform
+#   Σ_k is array of references to each Σ_i covariance matrix
+#   λ is regularization parameter in [0,1]
+function class_whiteners!{T<:AbstractFloat}(Σ_k::Array{Array{T,2},1}, γ::T = zero(T))
     for i = 1:length(Σ_k)
         tol = eps(T) * prod(size(Σ_k[i])) * maximum(Σ_k[i])
         Λ_i, V_i = LAPACK.syev!('V', 'U', Σ_k[i])  # Overwrite Σ_k with V such that VΛVᵀ = Σ_k
@@ -78,8 +118,11 @@ function class_whiteners!{T<:AbstractFloat}(Σ_k::Array{Array{T,2},1}, γ::T)
 end
 
 # Fit regularized discriminant model
-function rda!{T<:AbstractFloat,U<:AbstractFloat}(X::Matrix{T}, M::Matrix{T}, y::Vector{U}, λ::T,
-                                                 γ::T, n_k = class_counts(y))
+#   X in uncentered data matrix
+#   M is matrix of class means (one per row)
+#   y is one-based vector of class IDs
+function rda!{T<:AbstractFloat,U<:Integer}(X::Matrix{T}, M::Matrix{T}, y::Vector{U}, λ::T, γ::T, 
+                                           n_k = class_counts(y))
     k = length(n_k)
     H = center_rows!(X, M, y)
     # Scale Rows?
@@ -90,6 +133,20 @@ function rda!{T<:AbstractFloat,U<:AbstractFloat}(X::Matrix{T}, M::Matrix{T}, y::
     end
     W_k = class_whiteners!(Σ_k, γ)
 end
+
+#=
+function rda{T<:AbstractFloat,U<:Integer}(X::Matrix{T}, y::Vector{U}; M::Matrix{T} = 
+=#
+
+function row_sumsquares{T<:AbstractFloat}(X::Matrix{T})
+    n, p = size(X)
+    sumsquares = zeros(n)
+    for j = 1:p, i = 1:n
+        sumsquares[i] += X[i,j]^2
+    end
+    sumsquares
+end
+
 
 # %~%~%~%~%~%~%~%~%~% Helper Functions %~%~%~%~%~%~%~%~%
 
